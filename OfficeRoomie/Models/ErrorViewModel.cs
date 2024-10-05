e86e1cd26b79413aa58a931449c33f0df93753fb
eduar <eduar@HOMEPC>
2024-10-05 08:06:11 -0300

    Adicionar arquivos de projeto.

---
+++ OfficeRoomie/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace OfficeRoomie.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
